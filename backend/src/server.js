9204d51e9e3a77584699e020949f84013d1e56ff
bledian.p <bledianpotera1@gmail.com>
2026-02-08 17:27:21 +0100

    db connection, server

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running on port ${process.env.PORT || 3000}`)
+);
